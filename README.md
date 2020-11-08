89da8a773718f6ff9dd8d13a733079a85d108fed
serdar <serdar83durmus@gmail.com>
2020-11-08 14:43:00 +0100

    installasjonen av programmene er fullført (maps..)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# RestaurantApp_Map" 
+# RestaurantApp_Map
+- https://github.com/react-native-modal/react-native-modal
+- https://github.com/oblador/react-native-vector-icons
+- https://www.npmjs.com/package/react-native-axios
+- https://github.com/react-native-maps/react-native-maps
---
+++ RestaurantApp_Map/src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+// import Modal from 'react-native-modal';
+import {City, RestaurantDetail, SearchBar} from './components';
+
+const Main = (props) => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <Text>Selam</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Main;
